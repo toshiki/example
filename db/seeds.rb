7fe8b650db954169b8ffc123fab27b08fb25c788
tskst <tskst@users.noreply.github.com>
2017-11-10 19:42:46 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times.each do |i|
+  Station.create(name: "test#{i}")
+end
